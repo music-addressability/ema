e995fe5da19c02afc4f6b10369822b4f4e19c63a
raffazizzi <raffaeleviglianti@gmail.com>
2014-11-06 16:26:25 -0500

    first stab at parsing ranges (single item, range, mixed) and selecting a range of measures

--- Omas/omas/meislicer.py
+++ Omas/omas/meislicer.py
@@ -10,5 +10,29 @@ class MeiSlicer(object):
         self.beats = beats
         self.completeness = completeness
 
-    def slice(self):
-        return "sliced"
+    def parseRanges(self, ran):
+        groups = ran.split(",")
+        ranges = []
+        for g in groups:
+            ranges.append(g.split("-"))
+
+        return ranges
+
+    def getMeasures(self):
+        # There is always only one music element in an MEI doc
+        musicEl = self.meiDoc.getElementsByName("music")[0]
+        measures = musicEl.getDescendantsByName("measure")
+        # measure ranges will always return 1 item
+        mrange = self.parseRanges(self.measures)[0]
+
+        if len(mrange) == 1:
+            print measures[int(mrange[0])-1]
+            return "1 measure selected"
+        else:
+            selected_mm = []
+            for m_idx in range(int(mrange[0])-1, int(mrange[1])):
+                selected_mm.append(measures[m_idx])
+            print selected_mm
+            return "%i measures selected" % len(selected_mm)
+
+        return mrange
