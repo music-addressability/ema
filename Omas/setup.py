9b062860aedc62d4d8e18161587eeba1126e7ff0
raffazizzi <raffaeleviglianti@gmail.com>
2014-12-11 12:16:41 -0500

    added basic distutils package metadata

---
+++ Omas/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Omas',
+      version='0.1',
+      description='Open MEI Addressability Service',
+      author='Raff Viglianti',
+      url='https://github.com/umd-mith/ema',
+      packages=['omas'],
+     )
