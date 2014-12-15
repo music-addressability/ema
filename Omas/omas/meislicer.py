0eba28894893ac845e6b165f7088ca956b18c1d9
raffazizzi <raffaeleviglianti@gmail.com>
2014-12-15 16:13:45 -0500

    reorganized slicer class; better process to determine ranges for measures and staves; selection of staves

--- Omas/omas/meislicer.py
+++ Omas/omas/meislicer.py
@@ -5,34 +5,70 @@ class MeiSlicer(object):
     measures, staves, and beats."""
     def __init__(self, doc, measures, staves, beats, completeness):
         self.meiDoc = doc
-        self.measures = measures
-        self.staves = staves
-        self.beats = beats
+        self.requested_measures = measures
+        self.requested_staves = staves
+        self.requested_beats = beats
         self.completeness = completeness
 
-    def parseRanges(self, ran):
+    @property
+    def musicEl(self):
+        # There is always only one music element in an MEI doc
+        return self.meiDoc.getElementsByName("music")[0]
+
+    @property
+    def measures(self):
+        mm = self.musicEl.getDescendantsByName("measure")
+        selected = []
+        # measure ranges will always return 1 item
+        m_idxs = self._parseRanges(self.requested_measures)
+
+        for m_idx in m_idxs:
+            selected.append(mm[m_idx-1])
+
+        return selected
+
+    @property
+    def staves(self):
+        selected = {
+          "on"     : [],
+          "around" : []
+        }
+        s_nos = self._parseRanges(self.requested_staves)
+        for m in self.measures:
+            # If staff elements have @layer, use it to select the correct staff,
+            # otherwise default to element position order.
+            selected["on"].append(m.getDescendantsByName("staff"))
+            for el in m.getChildren():
+                if el.hasAttribute("staff"):
+                    if int(el.getAttribute("staff").getValue()) in s_nos:
+                        selected["around"].append(el)
+
+        return selected
+
+    # @property
+    # def beats(self):
+    #     selected = []
+    #     srange = self._parseRanges(self.requested_staves)
+    #     for m in self.measures:
+    #         # If staff elements have @layer, use it to select the correct staff,
+    #         # otherwise default to element position order.
+    #         selected.append(m.getDescendantsByName("staff"))
+
+    #     return selected
+
+    def _parseRanges(self, ran):
         groups = ran.split(",")
         ranges = []
+
         for g in groups:
-            ranges.append(g.split("-"))
+            values = g.split("-")
+            length = len(values)
+
+            if length == 1:
+                ranges.append(int(values[0]))
+            elif length == 2:
+                ranges += range(int(values[0]), int(values[1])+1)
+            else:
+                return "invalid range - return 500"
 
         return ranges
-
-    def getMeasures(self):
-        # There is always only one music element in an MEI doc
-        musicEl = self.meiDoc.getElementsByName("music")[0]
-        measures = musicEl.getDescendantsByName("measure")
-        # measure ranges will always return 1 item
-        mrange = self.parseRanges(self.measures)[0]
-
-        if len(mrange) == 1:
-            print measures[int(mrange[0])-1]
-            return "1 measure selected"
-        else:
-            selected_mm = []
-            for m_idx in range(int(mrange[0])-1, int(mrange[1])):
-                selected_mm.append(measures[m_idx])
-            print selected_mm
-            return "%i measures selected" % len(selected_mm)
-
-        return mrange
