9edf8f3b3b6b845eafc33a180fe8edf0fb8dbe95
raffazizzi <raffaeleviglianti@gmail.com>
2015-02-04 16:23:58 -0500

    added methods to locate staffDef context

--- Omas/omas/pymeiext.py
+++ Omas/omas/pymeiext.py
@@ -1,9 +1,8 @@
-from __future__ import print_function
 import six
 
 from pymei import MeiElement
 
-# Extend MeiElement with methods to deal with mixed content
+# Extend MeiElement
 
 def getChildrenNodes(self):
     """Return all children nodes, including text nodes"""
@@ -29,5 +28,75 @@ def getDecendantsTextNodes(self):
     _extractTextNode(self)
     return text_nodes
 
+def getStaffDefs(self):
+    """Return list of current staff definitions for the element's staff"""
+
+    # N.B. some elements may belong to more than one staff e.g. @staff="1 2"
+
+    staffDefs = []
+
+    def _look(el, val):
+        sd = el.lookback("staffDef")
+        if sd:
+            if sd.hasAttribute("n"):
+                if sd.getAttribute("n").getValue() == val:
+                    return sd
+                else:
+                    _look(sd, val)
+            else:
+                _look(sd, val)
+        else:
+            return None
+
+    # Get n if element is a staff
+    if self.getName() == "staff":
+        if self.hasAttribute("n"):
+            value = staff.getAttribute("n").getValue()
+            sd = _look(self, value)
+            if sd:
+                staffDefs.append(sd) 
+    # Then look for attribute
+    elif self.hasAttribute("staff"):
+        values = el.getAttribute("staff").getValue().split()
+        for v in values:
+            sd = _look(self, v)
+            if sd:
+                staffDefs.append(sd)
+    # Then staff ancestor element
+    elif self.getAncestor("staff"):
+        staff = self.getAncestor("staff")
+        if staff.hasAttribute("n"):
+            value = staff.getAttribute("n").getValue()
+            sd = _look(self, value)
+            if sd:
+                staffDefs.append(sd) 
+    # If the element does not belong to a staff, return all closest staff defs
+    else:
+        self.getClosestStaffDefs()
+
+    return staffDefs
+
+def getClosestStaffDefs(self):
+    """Return all the closest staff definitions from the element"""
+
+    stack = {}
+
+    # Emulate xpath preceding axis
+    doc = self.getDocument()
+    allEls = doc.getFlattenedTree()
+    preceding = allEls[:self.getPositionInDocument()]
+
+    for el in reversed(preceding): # Boost doens't allow extended slicing
+        if el.getName() == "staffDef":
+            if el.hasAttribute("n"):
+                val = el.getAttribute("n").getValue()
+                if val not in stack:
+                    stack[val] = el
+
+    # Return flattened stack object
+    return [stack[sd] for sd in stack]
+
 MeiElement.getChildrenNodes = getChildrenNodes
 MeiElement.getDecendantsTextNodes = getDecendantsTextNodes
+MeiElement.getStaffDefs = getStaffDefs
+MeiElement.getClosestStaffDefs = getClosestStaffDefs
