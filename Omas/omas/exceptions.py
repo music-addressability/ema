7925a5abb3a5c5718eeee56828aea98e7afce0ac
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-02-18 16:49:36 -0500

    Create dedicated exceptions file

---
+++ Omas/omas/exceptions.py
@@ -0,0 +1,16 @@
+
+class OmasException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class CannotReadMEIException(OmasException):
+    pass
+
+class CannotWriteMEIException(OmasException):
+    pass
+
+class UnknownMEIReadException(OmasException):
+    pass
+
+class BadApiRequest(OmasException):
+    pass
