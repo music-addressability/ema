91728ef9043777e71f520933625e4232e28d2cc7
raffazizzi <raffaeleviglianti@gmail.com>
2015-03-02 21:00:43 -0500

    adapted test to use nose framework and test api via a test server

--- Omas/tests/test_musdocinfo.py
+++ Omas/tests/test_musdocinfo.py
@@ -1,21 +1,19 @@
-import pytest
-
-import os
-from pymei import XmlImport
-from omas.meiinfo import MusDocInfo
+from omas import omas
+import json
 
 def test_DC_file():
-	meiDoc = XmlImport.documentFromFile(os.path.join("..", "data", "DC0101.mei"))
-	meiDocInfo = MusDocInfo(meiDoc)
-	assert len(meiDocInfo.measures) == 37
-	assert meiDocInfo.measure_labels == ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-		'11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
-		'25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37']
-	assert meiDocInfo.staves == {'0': ['Superius', 'Contratenor', 'Tenor', 'Bassus']}
-	assert meiDocInfo.beats == {"0": 4}
-@pytest.mark.full
-def test_MEI_samples():
-	path = os.path.join("..", "data", "MEISamples")
-	for mei in os.listdir(path):
-		meiDoc = XmlImport.documentFromFile(os.path.join(path, mei))
-		meiDocInfo = MusDocInfo(meiDoc)
+    test_omas = omas.test_client()
+    
+    url = "http%3A%2F%2Fdigitalduchemin.org%2Fmei%2FDC0101.xml/info.json"
+    resp = test_omas.get(url)
+    assert resp.status_code == 200
+    assert resp.mimetype == 'application/json'
+
+    data = json.loads(resp.data)
+
+    assert data["measures"] == 37
+    assert data["measure_labels"] == ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+        '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
+        '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37']
+    assert data["staves"] == {'0': ['Superius', 'Contratenor', 'Tenor', 'Bassus']}
+    assert data["beats"] == {"0": {"count": 4,"unit": 2}}
