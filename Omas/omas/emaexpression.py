3434846d593e7a926d73af25edb1dd2abf1a083c
raffazizzi <raffaeleviglianti@gmail.com>
2015-04-10 16:56:47 -0400

    Merging consecutive meausure ranges. This will make further processing simpler and will avoid unnecessary breaking of spanning elements (e.g. slurs). Also fixed a wrong assumption introduced with last revision: staves still need to be organized *by measure*, not just *by range*.

--- Omas/omas/emaexpression.py
+++ Omas/omas/emaexpression.py
@@ -1,5 +1,5 @@
 from omas.exceptions import BadApiRequest
-
+import itertools
 
 class EmaExpression(object):
     """ A class to parse an EMA expression given EMA information about a
@@ -14,30 +14,77 @@ class EmaExpression(object):
         # Re-organize a complex expression into a set of simpler ones.
         # For most EMA expressions this will result into just one expr.
 
-        mm_ranges = self.requested_measures.split(",")
+        # Compile measure ranges
+
+        end = str(self.docInfo["measures"])
+
+        all_mm = "1-{0}".format(end)
+
+        m_request = (
+            self.requested_measures
+            .replace("start", "1")
+            .replace("end", end)
+            .replace("all", all_mm)
+            )
+
+        mm_ranges = m_request.split(",")
+
+        # Determine consecutive ranges and merge them.
+        # e.g. 1-2,3 -> 1-3
+        # 2,3 -> 2-3
+        # 1-2,3-4 -> 1-4
+        ranges = []
+        for p in mm_ranges:
+            if '-' in p:
+                begin, end = map(int, p.split('-', 2))
+            else:
+                begin = end = int(p)
+            ranges.append([begin, end])
+
+        new_ranges = []
+
+        for r in ranges:
+            full = range(r[0], r[1]+1)
+            if len(new_ranges) == 0:
+                new_ranges.append(full)
+            else:
+                last_r = new_ranges[-1]
+                if last_r[-1] + 1 == r[0]:
+                    expanded = range(last_r[-1]+1,r[1]+1)
+                    new_ranges[-1] = last_r + expanded
+                else:
+                    new_ranges.append(full)
+
+        mm_ranges = new_ranges
         staves_by_m = self.requested_staves.split(",")
         beats_by_m = self.requested_beats.split(",")
 
-        # Only one measure range may need to be applied to all measures
+        # In staves and beats, only one measure range is applied to
+        # all measures
+        # So flatten measure lists to expand staves and beats when needed
+        merged = list(itertools.chain(*mm_ranges))
+
         if len(staves_by_m) == 1:
-            for x in mm_ranges[:-1]:
+            for x in merged[:-1]:
                 staves_by_m.append(staves_by_m[0])
         if len(beats_by_m) == 1:
-            for x in mm_ranges[:-1]:
+            for x in merged[:-1]:
                 beats_by_m.append(beats_by_m[0])
 
         # At this point, the ranges must match the number of measures
-        if len(staves_by_m) != len(mm_ranges) or \
-           len(beats_by_m) != len(mm_ranges):
+        if len(staves_by_m) != len(merged) or \
+           len(beats_by_m) != len(merged):
             e = "Requested staff/beat ranges do not match measure ranges."
             raise BadApiRequest(e)
 
         selections = []
-        for i, m_range in enumerate(mm_ranges):
+        for m_range in mm_ranges:
+            start = merged.index(m_range[0])
+            end = start + len(m_range)
             selection = EmaSingleRangeExpression(docInfo,
                                                  m_range,
-                                                 staves_by_m[i],
-                                                 beats_by_m[i])
+                                                 staves_by_m[start:end],
+                                                 beats_by_m[start:end])
             selections.append(selection)
 
         self.selections = selections
@@ -57,10 +104,10 @@ class EmaExpression(object):
             for m_i, m in enumerate(sel.measureRange):
                 ema_m = EmaMeasure(idx=int(m))
                 ema_staves = []
-                for s_i, s in enumerate(sel.staffRange):
+                for s_i, s in enumerate(sel.staffRanges[m_i]):
                     ema_s = EmaStaff(number=int(s), measure=ema_m)
                     ema_beat_ranges = []
-                    for br in sel.beatRange[m_i][s_i]:
+                    for br in sel.beatRanges[m_i][s_i]:
                         ema_beat_ranges.append(
                             EmaBeatRange(range_str=br,
                                          measure=ema_m,
@@ -87,27 +134,13 @@ class EmaSingleRangeExpression(object):
 
     @property
     def measureRange(self):
-        """Return list of indexes of requested measures"""
-
-        end = str(self.docInfo["measures"])
-
-        all_mm = "1-{0}".format(end)
-
-        request = (
-            self.requested_measures
-            .replace("start", "1")
-            .replace("end", end)
-            .replace("all", all_mm)
-            )
-
-        # store compiled expression
-        self.compiled_measures = request
-
-        return self._parseNumericRanges(request)
+        """Return list of indexes of requested measures"""  
+        self.compiled_measures = self.requested_measures
+        return self.requested_measures
 
     @property
-    def staffRange(self):
-        """Return list of indexes of requested staves"""
+    def staffRanges(self):
+        """Return list of indexes of requested staves, organized by measure"""
 
         # Locate number of staves defined at beggining of selection
         m = self.measureRange[0]-1
@@ -118,24 +151,28 @@ class EmaSingleRangeExpression(object):
 
         all_s = "1-{0}".format(end)
 
-        request = (
-            self.requested_staves
-            .replace("start", "1")
-            .replace("end", end)
-            .replace("all", all_s)
-            )
+        staves_by_m = []
 
-        # store compiled expression
-        self.compiled_staves = request
+        for measure in self.requested_staves:
 
-        staves = []
-        for r in request.split("+"):
-            staves = staves + self._parseNumericRanges(r)
+            request = (
+                measure
+                .replace("start", "1")
+                .replace("end", end)
+                .replace("all", all_s)
+                )
 
-        return staves
+            staves = []
+            for r in request.split("+"):
+                staves = staves + self._parseNumericRanges(r)
+            staves_by_m.append(staves)
+
+        self.compiled_staves = ",".join(self.requested_staves)
+
+        return staves_by_m
 
     @property
-    def beatRange(self):
+    def beatRanges(self):
         """Return list of request beats (timestamps) organized by
            staff and measure """
 
@@ -143,7 +180,7 @@ class EmaSingleRangeExpression(object):
         compiled_staves = []
 
         beats_by_measure = []
-        for m in self.measureRange:
+        for m_i, m in enumerate(self.measureRange):
             beats_by_staff = []
 
             # only replace wildcards here once you know the measure context
@@ -155,7 +192,7 @@ class EmaSingleRangeExpression(object):
             all_b = "1-{0}".format(end)
 
             ranges = (
-                self.requested_beats
+                self.requested_beats[m_i]
                 .replace("start", "1")
                 .replace("end", end)
                 .replace("all", all_b)
@@ -165,7 +202,7 @@ class EmaSingleRangeExpression(object):
             s_ranges = ranges.split("+")
 
             if len(s_ranges) == 1:
-                for x in self.staffRange[:-1]:
+                for x in self.staffRanges[m_i][:-1]:
                     s_ranges.append(s_ranges[0])
 
             compiled_staves.append("+".join(s_ranges))
@@ -180,7 +217,7 @@ class EmaSingleRangeExpression(object):
                 beats_by_staff.append(beats)
 
             # At this point, the ranges must match the number of staves
-            if len(beats_by_staff) != len(self.staffRange):
+            if len(beats_by_staff) != len(self.staffRanges[m_i]):
                 e = "Requested beat range does not match requested staff range"
                 raise BadApiRequest(e)
             beats_by_measure.append(beats_by_staff)
@@ -224,7 +261,7 @@ class EmaSingleRangeExpression(object):
 
     def getCompiled(self):
         # Kick off selection
-        self.beatRange
+        self.beatRanges
 
         return "{0}/{1}/{2}".format(self.compiled_measures,
                                     self.compiled_staves,
