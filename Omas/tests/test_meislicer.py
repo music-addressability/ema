4099e05b85663abc89a02d7059c096782b4a1cfa
raffazizzi <raffaeleviglianti@gmail.com>
2015-05-18 01:23:31 +0200

    reorganized tests a bit, still more to do

--- Omas/tests/test_meislicer.py
+++ Omas/tests/test_meislicer.py
@@ -1,20 +1,25 @@
 import unittest
 from omas import meiinfo
 from omas import meislicer
+import requests
+from urllib import unquote
 
 
 class MeiSlicerTest(unittest.TestCase):
-    # def setUp(self):
-    #     print('MeiSlicer Test Running')
-    #     f = open('tests/testfiles/attribute_syl.xml')
-    #     content = f.read()
-    #     f.close()
-    #     self.meidoc = meiinfo.read_MEI(content)
+    def setUp(self):
+        url = "http://music-encoding.org/sampleCollection/encodings/attribute_syl.xml"
+        r = requests.get(unquote(url), timeout=15)
+        self.meidoc = meiinfo.read_MEI(r.content).getMeiDocument()
 
-    # def test_slicer(self):
-    #     slicer = meislicer.MeiSlicer(self.meidoc, '1-1', '1,3', '1-1', None)
-    #     slicer.select()
-    #     print(slicer)
+    def test_slicer(self):
+        mei_slicer = meislicer.MeiSlicer(
+            self.meidoc,
+            '1',
+            'all',
+            '@all',
+            None
+        )
+        mei_slice = mei_slicer.slice()
 
     def tearDown(self):
         pass
