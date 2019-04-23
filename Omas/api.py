136748350965f3fed58a82704661ab7dd2a799b7
Ed Summers <ehs@pobox.com>
2019-04-23 14:01:32 -0400

    host/port
    Use OMAS_HOST and OMAS_PORT to optionally configure the host
    and port that omas will listen to.

--- Omas/api.py
+++ Omas/api.py
@@ -201,4 +201,6 @@ def information(meipath):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    host = os.environ.get('OMAS_HOST', '127.0.0.1')
+    port = os.environ.get('OMAS_PORT', 5000)
+    app.run(host=host, port=port, debug=True)
